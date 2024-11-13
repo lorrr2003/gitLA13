8b73fdd0965117a65c23ab91eb80553d4fafda3e
laurice <lauricejoymonic@gmail.com>
2024-11-13 18:59:03 +0800

    Add addrec function to program.py

--- program.py
+++ program.py
@@ -1,1 +1,32 @@
 # GitLA13 Project
+
+print("A - Add Record")
+print("B - View Records")
+print("C - Clear All Records")
+print("D - Exit")
+choice = ""
+while choice.upper() != 'D':
+    choice = input("ENTER SELECTION [A, B, C, or D]: ")
+    if choice.upper() == 'A':
+        print("Add Record")
+        addRec()
+    elif choice.upper() == 'B':
+        print("View Records")
+        viewRec()
+    elif choice.upper() == 'C':
+        print("Clear All Records")
+        clearRec()
+    elif choice.upper() == 'D':
+        print("Thank you!")
+
+    def addRec():
+        filename = "records.txt"  
+        name = input("Enter Name: ")
+        email = input("Enter Email: ")
+        addr = input("Enter Address: ")
+        
+        with open(filename, 'a') as file:
+            file.write(name + ", " + email + ", " + addr + "\n")
+            print("Record added successfully!")
+
+
